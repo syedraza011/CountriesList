25a846877f1cf529d6c8328c18532993db7715b4
Syed Raza <syed.raza011@gmail.com>
2023-07-22 11:19:45 -0400

    minor updates

--- CountriesList/ContentView.swift
+++ CountriesList/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 
+//struct ContentView: View {
+//    @StateObject var viewModel = CountriesViewModel()
+//
+//    var body: some View {
+//
+//        ForEach(viewModel.countries,id: \.self) { country in
+//            Text(country.name)
+//                .foregroundColor(.black)
+//        }
+//
+//
+//    }
+//        .onAppear {
+//            viewModel.getCountries()
+//        }
+//}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+import SwiftUI
+
 struct ContentView: View {
     @StateObject var viewModel = CountriesViewModel()
-    
+
     var body: some View {
-       
-            ForEach(viewModel.countries,id: \.self) { country in
-                Text(country.name)
+        VStack {
+            if viewModel.countries.isEmpty {
+                Text("Loading countries...")
                     .foregroundColor(.black)
+            } else {
+                List(viewModel.countries) { country in
+                    Text(country.name)
+                        .foregroundColor(.black)
+                }
             }
- 
+        }
         .padding()
+        .task {
+            await viewModel.getCountries()
+        }
     }
 }
 
