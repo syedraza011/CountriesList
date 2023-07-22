8f2cc2ef616bdc85ae93cd238dbb6a58dad6c5f8
Syed Raza <syed.raza011@gmail.com>
2023-07-22 10:46:48 -0400

    Initial Commit

---
+++ CountriesList/CountriesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesListApp.swift
+//  CountriesList
+//
+//  Created by Syed Raza on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CountriesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
